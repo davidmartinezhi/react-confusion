0db3e6c4de1ae35c35efe68b65e192ff6c9c9826
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2021-05-17 16:14:57 -0500

    Completed the basic structure for the Menu component

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,1 +1,16 @@
 import React, { component } from 'react';
+
+class Menu extends component{
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+
+        );
+    }
+}
+
+export default Menu;
