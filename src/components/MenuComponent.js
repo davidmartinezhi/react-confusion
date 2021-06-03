f52bbc129f767182df03f931dce9ec934f607bc1
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2021-06-02 19:59:49 -0500

    Made each item a card

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -12,16 +12,14 @@ class Menu extends Component {
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
-              <div key={dish.id} className="col-12 mt-5">
-                <Media tag="li">
-                  <Media left middle>
-                      <Media object src={dish.image} alt={dish.name} />
-                  </Media>
-                  <Media body className="ml-5">
-                    <Media heading>{dish.name}</Media>
-                    <p>{dish.description}</p>
-                  </Media>
-                </Media>
+              <div key={dish.id} className="col-12 col-md-5 m-1">
+                <Card tag="li">
+                  <CardImg width="100%" object src={dish.image} alt={dish.name} />
+                  <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
+                    
+                  </CardImgOverlay>
+                </Card>
               </div>
             );
         });
@@ -29,9 +27,7 @@ class Menu extends Component {
         return (
           <div className="container">
             <div className="row">
-              <Media list>
                   {menu}
-              </Media>
             </div>
           </div>
         );
