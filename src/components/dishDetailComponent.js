12dd30e5c81e4ad15c83f404884119104159bb8f
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2021-06-04 16:28:58 -0500

    Renamed the dish digital component

--- src/components/dishDetailComponent.js
+++ src/components/dishDetailComponent.js
@@ -1,3 +1,8 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
+class dishDigital extends Component{
+
+}
+
+export default dishDigital;
