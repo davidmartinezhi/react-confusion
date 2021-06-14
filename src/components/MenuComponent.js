72d8c2f83e10c63fa16e68a5aad4d310d93eb525
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2021-06-14 16:25:59 -0500

    Removed the DishDetail component

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -15,7 +15,7 @@ class Menu extends Component {
     const menu = this.props.dishes.map((dish) => {
             return (
               <div key={dish.id} className="col-12 col-md-5 m-1">
-                <Card onClick={() => this.onDishSelect(dish)}>
+                <Card onClick={() => this.props.onClick()}>
                   <CardImg width="100%" src={dish.image} alt={dish.name} />
                   <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
@@ -32,7 +32,6 @@ class Menu extends Component {
           {menu}
         </div>
         {/*Render the dish detail */ }
-        <DishDetail dish={this.state.selectedDish} />
       </div>
     );
   }
