bd2fde83f57a5adb99e36c47a0a2e0fbd9b6ed1b
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2021-06-15 14:31:36 -0500

    Removed the importation on component

--- src/components/dishDetailComponent.js
+++ src/components/dishDetailComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Card, CardBody, CardImg, CardImgOverlay, CardText, CardTitle } from "reactstrap";
 
 class DishDetail extends Component {
