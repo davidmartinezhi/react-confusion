5fe720159bf8238c92966763d9b0fc7c6fdfe18a
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2021-06-16 16:21:08 -0500

    Imported and Implementd the footer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent';
 import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
 
 class Main extends Component {
@@ -22,6 +23,7 @@ class Main extends Component {
             <Header />
             <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
             <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+            <Footer />
       </div>
     );
   }
