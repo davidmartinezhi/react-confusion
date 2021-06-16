6f2312fa4f1d990c88cc8ecdabf218065f924e2e
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2021-06-16 15:56:09 -0500

    Imported font awesome

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
