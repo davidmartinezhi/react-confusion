916981e657c21b5b6d2d46abb36229eb1e26493e
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2021-06-24 19:18:49 -0500

    Imported switch, route and redirect

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import DishDetail from './DishDetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import {Switch, Route, Redirect} from 'react-router-dom';
 
 class Main extends Component {
 
